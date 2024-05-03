3dccfa9777222488e172bb33ec4f499bd38384e7
Jonathan Gomez <gomezlemusj@gonzaga.org>
2024-05-03 13:11:15 -0400

    Basic app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('db.json');
+const port = 3000;
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
