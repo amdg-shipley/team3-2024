03296c3521ac7d19f6c946fa09225c8c7535a40e
Jonathan Gomez <gomezlemusj@gonzaga.org>
2024-05-07 10:18:59 -0400

    app.js updated

--- app.js
+++ app.js
@@ -5,11 +5,55 @@ const JSONdb = require('simple-json-db');
 const db = new JSONdb('db.json');
 const port = 3000;
 
-    
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlincluded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const Donutform = db.get('Donutform') || [];
+  res.render('Demo', { data: Donutform });
+});
+
+app.post('/Donut', (req, res) => {
+    Donutform.push((Flavor, Topping, Price));
+
+    db.set('Donutform', Donutform);
+
+    res.redirect('/')
+
+});
 
 
+app.get('/delete/:id', (req,res, next) => {
+  const id = req.params.id;
 
+  const Donutform = db.get('Donutform') || [];
+  Donutform.splice(id, 1);
+  db.set(`Donutform`, Donutform); 
+
+  res.redirect('/create');
+
+});
+
+app.get('/edit/:id', (req, res) => {
+  const id = req.params.id;
+
+  const Donutform = db.get('Donutform') || [];
+  Donutform.splice(id, 1);
+  db.set(`Donutform`, Donutform); 
+
+  res.redirect('/edit');
+
+});
+
+app.get('create', (req, res) => {
+  res.render('/home');
+
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
   });
+  
